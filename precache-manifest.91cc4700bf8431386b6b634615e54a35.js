d484e28d89df4584fbd002e8ec584a2847db13e0
Lawrence <lawrence891106@gmail.com>
2024-07-14 13:57:16 +0800

    Updates

---
+++ precache-manifest.91cc4700bf8431386b6b634615e54a35.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "247d1af0d0579931018853867db9bd75",
+    "url": "/Mern-Project/index.html"
+  },
+  {
+    "revision": "e065f49176ecc56050ca",
+    "url": "/Mern-Project/static/js/2.e3670c68.chunk.js"
+  },
+  {
+    "revision": "66081860786e9ca16045",
+    "url": "/Mern-Project/static/js/main.bc27ea62.chunk.js"
+  },
+  {
+    "revision": "11ac4c88e00f4cd8cfdc",
+    "url": "/Mern-Project/static/js/runtime~main.01289787.js"
+  }
+]);
